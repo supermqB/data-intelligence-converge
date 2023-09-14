815951f57c94c2db9e6e40a4c909207b9b8ced39
zhaohui <zhaohui@lrhealth.com>
2023-09-14 15:28:37 +0800

    feat: 多线程文件交互

---
+++ src/main/java/com/lrhealth/data/converge/common/util/file/FileUtils.java
@@ -0,0 +1,373 @@
+package com.lrhealth.data.converge.common.util.file;
+
+import cn.hutool.core.util.ArrayUtil;
+import cn.hutool.core.util.ZipUtil;
+import cn.hutool.crypto.SecureUtil;
+import cn.hutool.crypto.symmetric.AES;
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import static com.lrhealth.data.converge.scheduled.DownloadFileTask.FILE_SIZE;
+
+
+/**
+ * 文件处理辅助类
+ * @author zhaohui
+ * @version 1.0
+ */
+public class FileUtils {
+
+    /**
+     * 当前目录路径
+     */
+    public static String currentWorkDir = System.getProperty("user.dir") + "\\";
+
+    /**
+     * 左填充
+     *
+     * @param str
+     * @param length
+     * @param ch
+     * @return
+     */
+    public static String leftPad(String str, int length, char ch) {
+        if (str.length() >= length) {
+            return str;
+        }
+        char[] chs = new char[length];
+        Arrays.fill(chs, ch);
+        char[] src = str.toCharArray();
+        System.arraycopy(src, 0, chs, length - src.length, src.length);
+        return new String(chs);
+
+    }
+
+    /**
+     * 删除文件
+     *
+     * @param fileName
+     *            待删除的完整文件名
+     * @return
+     */
+    public static boolean delete(String fileName) {
+        boolean result = false;
+        File f = new File(fileName);
+        if (f.exists()) {
+            result = f.delete();
+
+        } else {
+            result = true;
+        }
+        return result;
+    }
+
+    /***
+     * 递归获取指定目录下的所有的文件（不包括文件夹）
+     *
+     * @param dirPath
+     * @return
+     */
+    public static ArrayList<File> getAllFiles(String dirPath) {
+        File dir = new File(dirPath);
+
+        ArrayList<File> files = new ArrayList<File>();
+
+        if (dir.isDirectory()) {
+            File[] fileArr = dir.listFiles();
+            for (int i = 0; i < fileArr.length; i++) {
+                File f = fileArr[i];
+                if (f.isFile()) {
+                    files.add(f);
+                } else {
+                    files.addAll(getAllFiles(f.getPath()));
+                }
+            }
+        }
+        return files;
+    }
+
+    /**
+     * 获取指定目录下的所有文件(不包括子文件夹)
+     *
+     * @param dirPath
+     * @return
+     */
+    public static ArrayList<File> getDirFiles(String dirPath) {
+        File path = new File(dirPath);
+        File[] fileArr = path.listFiles();
+        ArrayList<File> files = new ArrayList<File>();
+
+        for (File f : fileArr) {
+            if (f.isFile()) {
+                files.add(f);
+            }
+        }
+        return files;
+    }
+
+    /**
+     * 获取指定目录下特定文件后缀名的文件列表(不包括子文件夹)
+     *
+     * @param dirPath
+     *            目录路径
+     * @param suffix
+     *            文件后缀
+     * @return
+     */
+    public static ArrayList<File> getDirFiles(String dirPath,
+                                              final String suffix) {
+        File path = new File(dirPath);
+        File[] fileArr = path.listFiles(new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                String lowerName = name.toLowerCase();
+                String lowerSuffix = suffix.toLowerCase();
+                if (lowerName.endsWith(lowerSuffix)) {
+                    return true;
+                }
+                return false;
+            }
+
+        });
+        ArrayList<File> files = new ArrayList<File>();
+
+        for (File f : fileArr) {
+            if (f.isFile()) {
+                files.add(f);
+            }
+        }
+        return files;
+    }
+
+    /**
+     * 读取文件内容
+     *
+     * @param fileName
+     *            待读取的完整文件名
+     * @return 文件内容
+     * @throws IOException
+     */
+    public static String read(String fileName) throws IOException {
+        File f = new File(fileName);
+        FileInputStream fs = new FileInputStream(f);
+        String result = null;
+        byte[] b = new byte[fs.available()];
+        fs.read(b);
+        fs.close();
+        result = new String(b);
+        return result;
+    }
+
+    /**
+     * 写文件
+     *
+     * @param fileName
+     *            目标文件名
+     * @param fileContent
+     *            写入的内容
+     * @return
+     * @throws IOException
+     */
+    public static boolean write(String fileName, String fileContent)
+            throws IOException {
+        boolean result = false;
+        File f = new File(fileName);
+        FileOutputStream fs = new FileOutputStream(f);
+        byte[] b = fileContent.getBytes();
+        fs.write(b);
+        fs.flush();
+        fs.close();
+        result = true;
+        return result;
+    }
+
+    /**
+     * 追加内容到指定文件
+     *
+     * @param fileName
+     * @param fileContent
+     * @return
+     * @throws IOException
+     */
+    public static boolean append(String fileName, String fileContent)
+            throws IOException {
+        boolean result = false;
+        File f = new File(fileName);
+        if (f.exists()) {
+            RandomAccessFile rFile = new RandomAccessFile(f, "rw");
+            byte[] b = fileContent.getBytes();
+            long originLen = f.length();
+            rFile.setLength(originLen + b.length);
+            rFile.seek(originLen);
+            rFile.write(b);
+            rFile.close();
+        }
+        result = true;
+        return result;
+    }
+
+    /**
+     * 拆分文件
+     *
+     * @param fileName 待拆分的完整文件名
+     * @param byteSize 按多少字节大小拆分
+     * @param aesKey
+     * @return 拆分后的文件名列表
+     * @throws IOException
+     */
+    public List<String> splitBySize(String fileName, int byteSize, byte[] aesKey)
+            throws IOException {
+        List<String> parts = new ArrayList<String>();
+        File file = new File(fileName);
+        int count = (int) Math.ceil(file.length() / (double) byteSize);
+        int countLen = (count + "").length();
+        ThreadPoolExecutor threadPool = new ThreadPoolExecutor(count,
+                count * 3, 1, TimeUnit.SECONDS,
+                new ArrayBlockingQueue<Runnable>(count * 2));
+
+        for (int i = 0; i < count; i++) {
+            String partFileName = file.getName() + "."
+                    + leftPad((i + 1) + "", countLen, '0') + ".part";
+            threadPool.execute(new SplitRunnable(byteSize, i * byteSize,
+                    partFileName, file, aesKey));
+            parts.add(partFileName);
+        }
+        return parts;
+    }
+
+    /**
+     * 合并文件
+     *
+     * @param dirPath        拆分文件所在目录名
+     * @param partFileSuffix 拆分文件后缀名
+     * @param partFileSize   拆分文件的字节数大小
+     * @param mergeFileName  合并后的文件名
+     * @param aesKey 加密
+     * @throws IOException
+     */
+    public void mergePartFiles(String dirPath, String partFileSuffix,
+                               int partFileSize, String mergeFileName, byte[] aesKey) throws IOException {
+        ArrayList<File> partFiles = FileUtils.getDirFiles(dirPath,
+                partFileSuffix);
+        Collections.sort(partFiles, new FileComparator());
+
+        RandomAccessFile randomAccessFile = new RandomAccessFile(mergeFileName,
+                "rw");
+//        randomAccessFile.setLength(partFileSize * (partFiles.size() - 1)
+//                + partFiles.get(partFiles.size() - 1).length());
+        randomAccessFile.close();
+
+        ThreadPoolExecutor threadPool = new ThreadPoolExecutor(
+                partFiles.size(), partFiles.size() * 3, 1, TimeUnit.SECONDS,
+                new ArrayBlockingQueue<Runnable>(partFiles.size() * 2));
+
+        for (int i = 0; i < partFiles.size(); i++) {
+            threadPool.execute(new MergeRunnable(i * partFileSize,
+                    mergeFileName, partFiles.get(i), aesKey));
+        }
+
+    }
+
+    /**
+     * 根据文件名，比较文件
+     *
+     * @author yjmyzz@126.com
+     *
+     */
+    private class FileComparator implements Comparator<File> {
+        public int compare(File o1, File o2) {
+            return o1.getName().compareToIgnoreCase(o2.getName());
+        }
+    }
+
+    /**
+     * 分割处理Runnable
+     *
+     * @author yjmyzz@126.com
+     *
+     */
+    private class SplitRunnable implements Runnable {
+        int byteSize;
+        String partFileName;
+        File originFile;
+        int startPos;
+        byte[] aesKey;
+
+        public SplitRunnable(int byteSize, int startPos, String partFileName,
+                             File originFile, byte[] aesKey) {
+            this.startPos = startPos;
+            this.byteSize = byteSize;
+            this.partFileName = partFileName;
+            this.originFile = originFile;
+            this.aesKey = aesKey;
+        }
+
+        public void run() {
+            RandomAccessFile rFile;
+            OutputStream os;
+            try {
+                rFile = new RandomAccessFile(originFile, "r");
+                byte[] b = new byte[byteSize];
+                rFile.seek(startPos);// 移动指针到每“段”开头
+                int s = rFile.read(b);
+                if(s < b.length){
+                    b = ArrayUtil.sub(b,0,s);
+                }
+                byte[] gzip = ZipUtil.gzip(b);
+                AES aes = SecureUtil.aes(aesKey);
+                byte[] encrypt = aes.encrypt(gzip);
+                os = new FileOutputStream(partFileName);
+                os.write(encrypt, 0, encrypt.length);
+                os.flush();
+                os.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 合并处理Runnable
+     *
+     * @author yjmyzz@126.com
+     *
+     */
+    private class MergeRunnable implements Runnable {
+        long startPos;
+        String mergeFileName;
+        File partFile;
+        byte[] aesKey;
+
+        public MergeRunnable(long startPos, String mergeFileName, File partFile, byte[] aesKey) {
+            this.startPos = startPos;
+            this.mergeFileName = mergeFileName;
+            this.partFile = partFile;
+            this.aesKey = aesKey;
+        }
+
+        public void run() {
+            RandomAccessFile rFile;
+            try {
+                rFile = new RandomAccessFile(mergeFileName, "rw");
+                rFile.seek(startPos);
+                FileInputStream fs = new FileInputStream(partFile);
+                byte[] b = new byte[fs.available()];
+                fs.read(b);
+                AES aes = SecureUtil.aes(aesKey);
+                byte[] decrypt = aes.decrypt(b);
+                byte[] bytes = ZipUtil.unGzip(decrypt);
+                fs.close();
+                rFile.write(bytes);
+                rFile.close();
+                int i = FILE_SIZE.get() + 1;
+                FILE_SIZE.set(i);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
---
+++ src/main/java/com/lrhealth/data/converge/scheduled/DownloadFileTask.java
@@ -0,0 +1,139 @@
+package com.lrhealth.data.converge.scheduled;
+
+import cn.hutool.core.codec.Base64Decoder;
+import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson.JSONObject;
+import com.lrhealth.data.converge.common.util.file.FileUtils;
+import com.lrhealth.data.converge.scheduled.dto.PreFileStatusDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.Executor;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+/**
+ * @author zhaohui
+ * @version 1.0
+ * 文件下载任务
+ */
+@Slf4j
+@Component
+@EnableScheduling
+public class DownloadFileTask {
+
+    public static CopyOnWriteArraySet<Integer> taskSet = new CopyOnWriteArraySet<>();
+
+    public static AtomicInteger FILE_SIZE = new AtomicInteger();
+
+
+    @Resource
+    private Executor threadPoolTaskExecutor;
+
+    private static final String url = "127.0.0.1:18081/file";
+
+    @Scheduled(cron = "0/30 * * * * *")
+    @Transactional
+    public void refreshFENodesStatus() {
+        //循环前置机
+        //更新状态
+        //添加任务
+    }
+
+    @PostConstruct
+    public void loadTaskData() {
+        taskSet.add(1);
+        CompletableFuture.runAsync(this::downloadFile, threadPoolTaskExecutor);
+    }
+
+    public void downloadFile() {
+        while (true) {
+            if (taskSet.size() > 0) {
+                //异步获取文件
+                for (Integer taskId : taskSet) {
+                    List<CompletableFuture<Void>> futureList = new ArrayList<>();
+                    System.out.println("通知拆分：" + LocalDateTime.now());
+                    //通知前置机文件拆分-压缩-加密
+                    String s = HttpUtil.get(url + "/prepareFiles/" + taskId, 10000);
+
+                    PreFileStatusDto preFileStatusDto;
+                    //查询拆分结果
+                    while (true) {
+                        String statusResponse = HttpUtil.get(url + "/prepareFiles/status/" + taskId, 10000);
+                        preFileStatusDto = JSONObject.parseObject(statusResponse, PreFileStatusDto.class);
+                        if ("1".equals(preFileStatusDto.getStatus())) {
+                            break;
+                        }
+                        try {
+                            Thread.sleep(3000);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                        System.out.println("轮询状态：" + LocalDateTime.now());
+                    }
+
+                    System.out.println("开始下载：" + LocalDateTime.now());
+                    //异步下载文件
+                    for (Map.Entry<String, Integer> entry : preFileStatusDto.getPartFileMap().entrySet()) {
+                        CompletableFuture<Void> future = CompletableFuture.supplyAsync(() -> {
+                            int i = 0;
+                            long l = HttpUtil.downloadFile(url + "/downloadFiles/" + entry.getKey(),
+                                    "C:\\work\\" + entry.getKey());
+                            while (l != entry.getValue() && i < 3) {
+                                l = HttpUtil.downloadFile(url + "/downloadFiles/" + entry.getKey(),
+                                        "C:\\work\\" + entry.getKey());
+                                i++;
+                            }
+                            return null;
+                        }, threadPoolTaskExecutor);
+                        futureList.add(future);
+                    }
+                    //同步结果-校验-重试
+                    List<Void> fileList = CompletableFuture.allOf(futureList.toArray(new CompletableFuture[0]))
+                            .thenApply(e -> futureList.stream().map(CompletableFuture::join)
+                                    .collect(Collectors.toList())).join();
+                    System.out.println("下载完成：" + LocalDateTime.now());
+                    // 文件解密-解压缩-合并
+                    FileUtils fileUtils = new FileUtils();
+                    FILE_SIZE.set(0);
+                    try {
+                        fileUtils.mergePartFiles("C:\\work\\", ".part",
+                                1024*1024*200, "C:\\work\\" + "new.csv", Base64Decoder.decode(s));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    while (true){
+                        if(FILE_SIZE.get() == preFileStatusDto.getPartFileMap().size()){
+                            System.out.println("合并完成：" + LocalDateTime.now());
+                            break;
+                        }
+                        try {
+                            Thread.sleep(3000);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+
+                taskSet.clear();
+            }
+        }
+    }
+}
