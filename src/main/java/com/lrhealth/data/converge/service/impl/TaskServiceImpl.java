f90976b484bd8e9c491fb6e6b2c443690e16c48e
jinmengyu <jinmengyu@lrhealth.com>
2023-10-09 10:07:50 +0800

    feature: 方法迁移

--- src/main/java/com/lrhealth/data/converge/service/impl/TaskServiceImpl.java
+++ src/main/java/com/lrhealth/data/converge/service/impl/TaskServiceImpl.java
@@ -2,25 +2,34 @@ package com.lrhealth.data.converge.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.text.CharSequenceUtil;
+import cn.hutool.core.util.IdUtil;
 import cn.hutool.core.util.ObjectUtil;
 import com.alibaba.fastjson.JSON;
 import com.lrhealth.data.common.enums.conv.ConvergeTypeEnum;
 import com.lrhealth.data.common.enums.conv.KafkaSendFlagEnum;
+import com.lrhealth.data.common.enums.conv.XdsStatusEnum;
 import com.lrhealth.data.common.exception.CommonException;
+import com.lrhealth.data.converge.common.util.file.LargeFileUtil;
+import com.lrhealth.data.converge.dao.adpter.JDBCRepository;
 import com.lrhealth.data.converge.dao.entity.Xds;
-import com.lrhealth.data.converge.dao.service.ConvergeConfigService;
-import com.lrhealth.data.converge.dao.service.FrontendService;
+import com.lrhealth.data.converge.dao.service.XdsService;
 import com.lrhealth.data.converge.model.*;
+import com.lrhealth.data.converge.scheduled.dao.entity.ConvTask;
+import com.lrhealth.data.converge.scheduled.dao.entity.ConvTaskResultView;
+import com.lrhealth.data.converge.scheduled.dao.service.ConvTaskResultViewService;
+import com.lrhealth.data.converge.scheduled.dao.service.ConvTaskService;
 import com.lrhealth.data.converge.service.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 
@@ -42,11 +51,7 @@ public class TaskServiceImpl implements TaskService {
     @Resource
     private KafkaTemplate<String, String> kafkaTemplate;
     @Resource
-    private ConvergeConfigService configService;
-    @Resource
     private FepService fepService;
-    @Resource
-    private FrontendService frontendService;
     @Resource
     private FileService fileService;
     @Resource
@@ -55,6 +60,14 @@ public class TaskServiceImpl implements TaskService {
     private DataXService dataXService;
     @Resource
     private ConvConfigService convConfigService;
+    @Resource
+    private ConvTaskResultViewService taskResultViewService;
+    @Resource
+    private XdsService xdsService;
+    @Resource
+    private ConvTaskService taskService;
+    @Resource
+    private JDBCRepository jdbcRepository;
 
     @Override
     public FileExecInfoDTO createTask(@RequestBody TaskDto taskDto) {
@@ -137,6 +150,61 @@ public class TaskServiceImpl implements TaskService {
             kafkaTemplate.send(topic, JSON.toJSONString(map));
             xdsInfoService.updateKafkaSent(xds);
         }
+    }
+
+
+    @Override
+    @Async
+    public void fileParseAndSave(Integer taskResultViewId) {
+        // 创建xds
+        Xds xds = createXds(taskResultViewId);
+        Integer countNumber = LargeFileUtil.csvParseAndInsert(xds.getStoredFilePath(), xds.getStoredFileName(), xds.getId(), xds.getOdsTableName());
+        // 获得数据的大概存储大小
+        String avgRowLength = getAvgRowLength(xds.getOdsTableName());
+        // 更新xds
+        updateXds(xds.getId(), countNumber * Long.parseLong(avgRowLength));
+        // 发送kafka
+        xdsSendKafka(xds);
+    }
+
+    private Xds createXds(Integer taskResultViewId){
+        ConvTaskResultView taskResultView = taskResultViewService.getById(taskResultViewId);
+        ConvTask convTask = taskService.getById(taskResultView.getTaskId());
+        Xds xds =  Xds.builder()
+                .id(IdUtil.getSnowflakeNextId())
+                .orgCode(convTask.getOrgCode())
+                .sysCode(convTask.getSysCode())
+                .convergeMethod(convTask.getConvergeMethod())
+                .dataConvergeStartTime(convTask.getStartTime())
+                .dataConvergeStatus(XdsStatusEnum.INIT.getCode())
+                .odsModelName(taskResultView.getTableName())
+                .oriFileName(taskResultView.getFeStoredFilename())
+                .storedFilePath(taskResultView.getStoredPath())
+                .storedFileName(taskResultView.getFeStoredFilename())
+                .storedFileType("csv")
+                .storedFileMode(0)
+                .odsTableName(convTask.getSysCode() + "_" + taskResultView.getTableName())
+                .storedFileSize(BigDecimal.valueOf(taskResultView.getDataSize()))
+                .dataCount(taskResultView.getDataItemCount())
+                .createTime(LocalDateTime.now())
+                .build();
+        xdsService.save(xds);
+        return xdsService.getById(xds.getId());
+    }
+
+    private void updateXds(Long xdsId, Long dataSize){
+        Xds updateXds = Xds.builder().id(xdsId).dataSize(dataSize)
+                .dataConvergeEndTime(LocalDateTime.now()).updateTime(LocalDateTime.now()).build();
+        xdsService.updateById(updateXds);
+    }
+
+    private String getAvgRowLength(String odsTableName){
+        // 刷新tables表的数据
+        String refreshSql = "ANALYZE TABLE " + odsTableName + " COMPUTE STATISTICS FOR ALL COLUMNS SIZE AUTO;";
+        jdbcRepository.execSql(refreshSql);
+        // 获取每行的平均大小
+        String selectSql = "select AVG_ROW_LENGTH from information_schema.TABLES where TABLE_NAME = '" + odsTableName + "';";
+        return jdbcRepository.execSql(selectSql);
     }
 
 }
