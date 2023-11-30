ca048ca92991da9bd50f21e5a8cac2010d42c605
jinmengyu <jinmengyu@lrhealth.com>
2023-11-30 21:18:04 +0800

    feature: 增量字段查询

---
+++ src/main/java/com/lrhealth/data/converge/scheduled/dao/service/impl/ConvCollectFieldServiceImpl.java
@@ -0,0 +1,15 @@
+package com.lrhealth.data.converge.scheduled.dao.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.lrhealth.data.converge.scheduled.dao.entity.ConvCollectField;
+import com.lrhealth.data.converge.scheduled.dao.mapper.ConvCollectFieldMapper;
+import com.lrhealth.data.converge.scheduled.dao.service.ConvCollectFieldService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author lei
+ * @Date: 2023/11/30/18:33
+ */
+@Service
+public class ConvCollectFieldServiceImpl extends ServiceImpl<ConvCollectFieldMapper, ConvCollectField> implements ConvCollectFieldService {
+}
