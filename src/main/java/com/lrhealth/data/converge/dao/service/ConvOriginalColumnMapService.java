5e87c77da253b6487eba84788e42f64c3c9c26ce
flzhang <zhangfeilong@lrhealth.com>
2024-01-16 16:24:07 +0800

    fix: 数据库同步时创建表结构映射表信息

---
+++ src/main/java/com/lrhealth/data/converge/dao/service/ConvOriginalColumnMapService.java
@@ -0,0 +1,17 @@
+package com.lrhealth.data.converge.dao.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.lrhealth.data.converge.dao.entity.ConvOriginalColumn;
+import com.lrhealth.data.converge.dao.entity.ConvOriginalColumnMap;
+
+/**
+ * <p>
+ * 客户端库表字段信息 服务类
+ * </p>
+ *
+ * @author jinmengyu
+ * @since 2024-01-03
+ */
+public interface ConvOriginalColumnMapService extends IService<ConvOriginalColumnMap> {
+
+}
---
+++ src/main/java/com/lrhealth/data/converge/dao/service/ConvOriginalTableMapService.java
@@ -0,0 +1,17 @@
+package com.lrhealth.data.converge.dao.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.lrhealth.data.converge.dao.entity.ConvOriginalTable;
+import com.lrhealth.data.converge.dao.entity.ConvOriginalTableMap;
+
+/**
+ * <p>
+ * 客户端库表信息 服务类
+ * </p>
+ *
+ * @author jinmengyu
+ * @since 2024-01-03
+ */
+public interface ConvOriginalTableMapService extends IService<ConvOriginalTableMap> {
+
+}
