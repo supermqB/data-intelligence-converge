c2d8f47a4a8f9242b27cf9195b778e1c921f7004
flzhang <zhangfeilong@lrhealth.com>
2024-01-23 16:44:55 +0800

    fix: 读取客户端表生成数据时字段类型改为目标库数据类型

---
+++ src/main/java/com/lrhealth/data/converge/dao/service/ConvFieldTypeService.java
@@ -0,0 +1,16 @@
+package com.lrhealth.data.converge.dao.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.lrhealth.data.converge.dao.entity.ConvFieldType;
+
+/**
+ * <p>
+ * 数据源字段类型映射
+ * </p>
+ *
+ * @author flzhang
+ * @since 2024-01-23
+ */
+public interface ConvFieldTypeService extends IService<ConvFieldType> {
+
+}
