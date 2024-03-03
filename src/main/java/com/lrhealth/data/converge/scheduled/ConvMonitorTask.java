605e415df03820d9a669236c1e8f8775f957b921
zhuanning <1qaz2wsx>
2024-03-03 18:03:00 +0800

    feat:汇聚新增行政区划编码,发送消息新增key

--- src/main/java/com/lrhealth/data/converge/scheduled/ConvMonitorTask.java
+++ src/main/java/com/lrhealth/data/converge/scheduled/ConvMonitorTask.java
@@ -24,6 +24,7 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 汇聚监测信息定时任务
@@ -43,24 +44,50 @@ public class ConvMonitorTask implements CommandLineRunner {
     private ConvOdsDatasourceConfigMapper convOdsDatasourceConfigMapper;
     @Resource
     private ConvMonitorService convMonitorService;
+    @Value("${region.current.code}")
+    private String regionCode;
 
+    private static final String INCREMENT_TYPE_KEY = "INCREMENT_TYPE_KEY";
+
+    private static final String MIRROR_DB_TYPE_KEY = "MIRROR_DB_TYPE_KEY";
     @Override
     public void run(String... args) throws Exception {
         monitorIncrementTask();
         targetDataBaseMonitorTask();
+        mirrorDataBaseMonitorTask();
     }
 
     /**
-     * 定时发送镜像库数据源信息至前置机
+     * 定时发送增量数据监测状态
      */
     @Scheduled(cron = "0 0/30 * * * ?")
     public void monitorIncrementTask() {
-        List<ConvOdsDatasourceConfig> mirrorDbList = convOdsDatasourceConfigMapper.selectList(new LambdaQueryWrapper<ConvOdsDatasourceConfig>().eq(ConvOdsDatasourceConfig::getDelFlag, 0)
-                .eq(ConvOdsDatasourceConfig::getDsType, 2));
+        List<ConvOdsDatasourceConfig> mirrorDbList = getDataSourceConfigByType((short) 2);
         if (CollectionUtils.isEmpty(mirrorDbList)) {
             return;
         }
-        kafkaTemplate.send(incrDataTopic, JSON.toJSONString(mirrorDbList));
+        kafkaTemplate.send(incrDataTopic,INCREMENT_TYPE_KEY, JSON.toJSONString(mirrorDbList));
+    }
+
+    /**
+     * 镜像库连接状态监控
+     */
+    @Scheduled(cron = "0 0/5 * * * ?")
+    public void mirrorDataBaseMonitorTask() {
+        List<ConvOdsDatasourceConfig> mirrorDbList = getDataSourceConfigByType((short) 2);
+        if (CollectionUtils.isEmpty(mirrorDbList)) {
+            return;
+        }
+        kafkaTemplate.send(incrDataTopic,MIRROR_DB_TYPE_KEY, JSON.toJSONString(mirrorDbList));
+    }
+
+    private List<ConvOdsDatasourceConfig> getDataSourceConfigByType(short dsType){
+         List<ConvOdsDatasourceConfig> datasourceConfigs = convOdsDatasourceConfigMapper.selectList(new LambdaQueryWrapper<ConvOdsDatasourceConfig>().eq(ConvOdsDatasourceConfig::getDelFlag, 0)
+                .eq(ConvOdsDatasourceConfig::getDsType, dsType));
+         if (CollectionUtils.isNotEmpty(datasourceConfigs)){
+             return datasourceConfigs.stream().filter(dsConfig -> regionCode.equals(dsConfig.getOrgCode().substring(1, regionCode.length() + 1))).collect(Collectors.toList());
+         }
+         return null;
     }
 
     /**
@@ -68,15 +95,13 @@ public class ConvMonitorTask implements CommandLineRunner {
      */
     @Scheduled(cron = "0 0/5 * * * ?")
     public void targetDataBaseMonitorTask() {
-        List<ConvOdsDatasourceConfig> targetDbList = convOdsDatasourceConfigMapper.selectList(new LambdaQueryWrapper<ConvOdsDatasourceConfig>().eq(ConvOdsDatasourceConfig::getDelFlag, 0)
-                .eq(ConvOdsDatasourceConfig::getDsType, 1));
+        final List<ConvOdsDatasourceConfig> targetDbList = getDataSourceConfigByType((short) 1);
         if (CollectionUtils.isEmpty(targetDbList)) {
             return;
         }
         targetDbList.forEach(dsConf -> {
             dbLinkAndHttpPost(MonitorMsg.MsgTypeEnum.WRITER_DB_CHECK,dsConf);
         });
-
     }
 
     public void dbLinkAndHttpPost(MonitorMsg.MsgTypeEnum msgTypeEnum, ConvOdsDatasourceConfig dsConfig) {
