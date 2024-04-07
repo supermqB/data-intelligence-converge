7ff92803dac406be572d7dd72e516935ba29611f
zhuanning <1qaz2wsx>
2024-04-07 09:57:12 +0800

    refactor:采集标准hive-类型全转换为Varchar

--- src/main/java/com/lrhealth/data/converge/service/impl/FeTunnelConfigServiceImpl.java
+++ src/main/java/com/lrhealth/data/converge/service/impl/FeTunnelConfigServiceImpl.java
@@ -62,6 +62,9 @@ public class FeTunnelConfigServiceImpl implements FeTunnelConfigService {
     @Resource
     private ModelConfigService modelConfigService;
 
+    @Resource
+    private ConvOriginalTableService convOriginalTableService;
+
     @Value("${file-collect.structure-type}")
     private String structureTypeStr;
 
@@ -69,7 +72,7 @@ public class FeTunnelConfigServiceImpl implements FeTunnelConfigService {
     @Override
     public List<TunnelMessageDTO> getFepTunnelConfig(String ip, Integer port) {
         List<TunnelMessageDTO> messageDTOList = new ArrayList<>();
-        if (ip.equals(System.getProperty("converge.ip"))){
+        if (ip.equals(System.getProperty("converge.ip"))) {
             directTunnelConfig(messageDTOList);
             return messageDTOList;
         }
@@ -97,8 +100,8 @@ public class FeTunnelConfigServiceImpl implements FeTunnelConfigService {
     @Override
     public void kafkaUpdateFepStatus(String key, String msgBody) {
         if (CharSequenceUtil.isBlank(key)) return;
-        log.info("kafkaUpdateFepStatus,key={}",key);
-        switch (key){
+        log.info("kafkaUpdateFepStatus,key={}", key);
+        switch (key) {
             case "tunnel":
                 TunnelStatusKafkaDto dto = JSON.parseObject(msgBody, TunnelStatusKafkaDto.class);
                 feNodeService.updateTunnel(dto);
@@ -140,14 +143,14 @@ public class FeTunnelConfigServiceImpl implements FeTunnelConfigService {
     }
 
 
-    private List<ConvFeNode> getFepListByIpAndPort(String ip, Integer port){
-        if (CharSequenceUtil.isBlank(ip)){
+    private List<ConvFeNode> getFepListByIpAndPort(String ip, Integer port) {
+        if (CharSequenceUtil.isBlank(ip)) {
             throw new CommonException("请输入前置机ip地址");
         }
         List<ConvFeNode> fepList = convFeNodeService.list(new LambdaQueryWrapper<ConvFeNode>().eq(ConvFeNode::getIp, ip)
                 .eq((port != null), ConvFeNode::getPort, port)
                 .eq(ConvFeNode::getDelFlag, 0));
-        if (CollUtil.isEmpty(fepList)){
+        if (CollUtil.isEmpty(fepList)) {
             return Collections.emptyList();
         }
         return fepList;
@@ -155,11 +158,12 @@ public class FeTunnelConfigServiceImpl implements FeTunnelConfigService {
 
     /**
      * 直连管道配置
+     *
      * @param messageDTOList
      */
-    public void directTunnelConfig(List<TunnelMessageDTO> messageDTOList){
+    public void directTunnelConfig(List<TunnelMessageDTO> messageDTOList) {
         List<ConvTunnel> tunnelList = tunnelService.list(new LambdaQueryWrapper<ConvTunnel>().eq(ConvTunnel::getFrontendId, -1));
-        if (CollUtil.isEmpty(tunnelList)){
+        if (CollUtil.isEmpty(tunnelList)) {
             return;
         }
         tunnelList.forEach(tunnel -> {
@@ -168,9 +172,9 @@ public class FeTunnelConfigServiceImpl implements FeTunnelConfigService {
         });
     }
 
-    private void feNodeTunnelConfig(List<TunnelMessageDTO> messageDTOList, ConvFeNode fep){
+    private void feNodeTunnelConfig(List<TunnelMessageDTO> messageDTOList, ConvFeNode fep) {
         List<ConvTunnel> tunnelList = tunnelService.list(new LambdaQueryWrapper<ConvTunnel>().eq(ConvTunnel::getFrontendId, fep.getId()));
-        if (CollUtil.isEmpty(tunnelList)){
+        if (CollUtil.isEmpty(tunnelList)) {
             return;
         }
         tunnelList.forEach(tunnel -> {
@@ -180,7 +184,7 @@ public class FeTunnelConfigServiceImpl implements FeTunnelConfigService {
     }
 
     @Override
-    public TunnelMessageDTO getTunnelMessage(ConvTunnel tunnel){
+    public TunnelMessageDTO getTunnelMessage(ConvTunnel tunnel) {
         TunnelMessageDTO tunnelMessageDTO = new TunnelMessageDTO();
         // 管道基本信息
         // todo: 用普通的convert替换beanutil
@@ -188,7 +192,7 @@ public class FeTunnelConfigServiceImpl implements FeTunnelConfigService {
         tunnelMessageDTO.setTimeDif(getCronTimeUnit(tunnel.getTimeDif(), tunnel.getTimeUnit()));
         tunnelMessageDTO.setDependenceTunnelId(CharSequenceUtil.isNotBlank(tunnel.getDependenceTunnelId()) ? Long.valueOf(tunnel.getDependenceTunnelId()) : null);
         TunnelCMEnum tunnelCMEnum = TunnelCMEnum.of(tunnel.getConvergeMethod());
-        switch (Objects.requireNonNull(tunnelCMEnum)){
+        switch (Objects.requireNonNull(tunnelCMEnum)) {
             case LIBRARY_TABLE:
             case CDC_LOG:
                 tunnelJdbcMessage(tunnel, tunnelMessageDTO);
@@ -204,7 +208,7 @@ public class FeTunnelConfigServiceImpl implements FeTunnelConfigService {
         return tunnelMessageDTO;
     }
 
-    private void tunnelJdbcMessage(ConvTunnel tunnel, TunnelMessageDTO tunnelMessageDTO){
+    private void tunnelJdbcMessage(ConvTunnel tunnel, TunnelMessageDTO tunnelMessageDTO) {
         // 库表/日志的读库信息
         JdbcInfoDto jdbcInfoDto = new JdbcInfoDto();
         ConvOdsDatasourceConfig readerDs = odsDatasourceConfigService.getById(tunnel.getReaderDatasourceId());
@@ -214,7 +218,7 @@ public class FeTunnelConfigServiceImpl implements FeTunnelConfigService {
         jdbcInfoDto.setDbPasswd(readerDs.getDsPwd());
         jdbcInfoDto.setDbSchema(readerDs.getSchema());
         // 库表采集
-        if (tunnel.getConvergeMethod().equals(LIBRARY_TABLE.getCode())){
+        if (tunnel.getConvergeMethod().equals(LIBRARY_TABLE.getCode())) {
             // 全量/增量采集
             jdbcInfoDto.setColType(tunnel.getColType());
             jdbcInfoDto.setFullColStartTime(String.valueOf(tunnel.getFullColStartTime()));
@@ -225,7 +229,7 @@ public class FeTunnelConfigServiceImpl implements FeTunnelConfigService {
             jdbcInfoDto.setColTableType(tunnel.getColTableType());
             // 库到库
             String dbType = null;
-            if(LibraryTableModelEnum.DATABASE_TO_DATABASE.getCode().equals(jdbcInfoDto.getCollectModel())){
+            if (LibraryTableModelEnum.DATABASE_TO_DATABASE.getCode().equals(jdbcInfoDto.getCollectModel())) {
                 ConvOdsDatasourceConfig writerDs = odsDatasourceConfigService.getById(tunnel.getWriterDatasourceId());
                 jdbcInfoDto.setJdbcUrlForIn(writerDs.getDsUrl());
                 jdbcInfoDto.setDbUserNameForIn(writerDs.getDsUsername());
@@ -234,14 +238,14 @@ public class FeTunnelConfigServiceImpl implements FeTunnelConfigService {
                 dbType = writerDs.getDbType();
             }
             // 表以及对应的sql信息
-            assembleTableInfoMessage(tunnel, jdbcInfoDto,"HDFS".equals(dbType));
+            assembleTableInfoMessage(tunnel, jdbcInfoDto, "HDFS".equals(dbType));
         }
 
-        if (tunnel.getConvergeMethod().equals(CDC_LOG.getCode())){
+        if (tunnel.getConvergeMethod().equals(CDC_LOG.getCode())) {
             String collectRange = tunnel.getCollectRange();
             List<TableInfoDto> tableInfoDtoList = new ArrayList<>();
             String[] split = collectRange.split(",");
-            for (String table : split){
+            for (String table : split) {
                 TableInfoDto tableInfoDto = new TableInfoDto();
                 tableInfoDto.setTableName(table);
                 tableInfoDtoList.add(tableInfoDto);
@@ -251,7 +255,7 @@ public class FeTunnelConfigServiceImpl implements FeTunnelConfigService {
         tunnelMessageDTO.setJdbcInfoDto(jdbcInfoDto);
     }
 
-    private void tunnelFileMessage(ConvTunnel tunnel, TunnelMessageDTO tunnelMessageDTO){
+    private void tunnelFileMessage(ConvTunnel tunnel, TunnelMessageDTO tunnelMessageDTO) {
         FileCollectInfoDto fileCollectInfoDto = new FileCollectInfoDto();
         // 文件采集目录
         fileCollectInfoDto.setFileModeCollectDir(tunnel.getFileModeCollectDir());
@@ -262,9 +266,9 @@ public class FeTunnelConfigServiceImpl implements FeTunnelConfigService {
         List<String> fileSuffixList = new ArrayList<>();
         Integer fileStorageMode = tunnel.getFileStorageMode();
         FileStorageTypeEnum type = FileStorageTypeEnum.of(fileStorageMode);
-        switch (Objects.requireNonNull(type)){
+        switch (Objects.requireNonNull(type)) {
             case DATABASE:
-                if (CharSequenceUtil.isNotBlank(structureTypeStr)){
+                if (CharSequenceUtil.isNotBlank(structureTypeStr)) {
                     fileSuffixList.addAll(Lists.newArrayList(structureTypeStr.split(",")));
                 }
                 break;
@@ -280,34 +284,44 @@ public class FeTunnelConfigServiceImpl implements FeTunnelConfigService {
     }
 
 
-    private void assembleTableInfoMessage(ConvTunnel tunnel, JdbcInfoDto jdbcInfoDto,Boolean isHive){
+    private void assembleTableInfoMessage(ConvTunnel tunnel, JdbcInfoDto jdbcInfoDto, Boolean isHive) {
         // 库表采集范围和sql查询语句
         List<TableInfoDto> tableInfoDtoList = new ArrayList<>();
         List<String> tableList = Arrays.asList(tunnel.getCollectRange().split(","));
         // 增量字段
         List<ConvCollectField> collectFieldList = collectFieldService.list(new LambdaQueryWrapper<ConvCollectField>()
-                        .in(ConvCollectField::getTableName, tableList)
-                        .eq(ConvCollectField::getTunnelId, tunnel.getId())
-                        .eq(ConvCollectField::getSystemCode, tunnel.getSysCode()));
+                .in(ConvCollectField::getTableName, tableList)
+                .eq(ConvCollectField::getTunnelId, tunnel.getId())
+                .eq(ConvCollectField::getSystemCode, tunnel.getSysCode()));
         Map<String, List<OriginalModelColumn>> modelColumnMap = new HashMap<>(collectFieldList.size());
         Map<String, OriginalModel> hdfsMap = new HashMap<>();
         Map<String, String> hiveConfigMap = new HashMap<>();
-        if (isHive){
+        Map<Long, ConvOriginalTable> originalTableMap = null;
+        if (isHive) {
             List<Long> modelIdList = new ArrayList<>();
             for (ConvCollectField collectField : collectFieldList) {
                 List<OriginalModelColumn> modelColumns = odsModelService.getColumnList(collectField.getTableName(), collectField.getSystemCode());
-                modelColumnMap.put(collectField.getTableName(),modelColumns);
+                modelColumnMap.put(collectField.getTableName(), modelColumns);
                 modelIdList.add(modelColumns.get(0).getModelId());
             }
             List<OriginalModel> modelList = odsModelService.getModelList(modelIdList);
+            //原始模型Map modelNameEn-model
             hdfsMap = modelList.stream().filter(e -> StringUtils.isNotEmpty(e.getStoragePath())).collect(Collectors.toMap(OriginalModel::getNameEn, e -> e, (m1, m2) -> m1));
-            //查询hive配置
             List<ModelConfig> modelConfigs = modelConfigService.list(new LambdaQueryWrapper<ModelConfig>().in(ModelConfig::getModelId, modelIdList));
+            //Hive表配置Map modelNameEn-数据存储类型
             hiveConfigMap = modelConfigs.stream().filter(e -> StringUtils.isNotEmpty(e.getTableType())).collect(Collectors.toMap(ModelConfig::getTableName, ModelConfig::getTableType));
-
+            List<Long> originalIdList = hdfsMap.values().stream().map(OriginalModel::getOriginalId).collect(Collectors.toList());
+            //查询原始结构
+            if (CollectionUtil.isNotEmpty(originalIdList)) {
+                List<ConvOriginalTable> originalTables = convOriginalTableService.list(new LambdaQueryWrapper<ConvOriginalTable>()
+                        .in(ConvOriginalTable::getId, originalIdList)
+                        .eq(ConvOriginalTable::getDelFlag, 0));
+                originalTableMap = originalTables.stream().collect(Collectors.toMap(ConvOriginalTable::getId, e -> e, (m1, m2) -> m1));
+            }
         }
-        Map<String, OriginalModel> finalHdfsMap = hdfsMap;
         Map<String, String> finalHiveConfigMap = hiveConfigMap;
+        Map<Long, ConvOriginalTable> finalOriginalTableMap = originalTableMap;
+        Map<String, OriginalModel> finalHdfsMap = hdfsMap;
         collectFieldList.forEach(model -> {
             TableInfoDto tableInfoDto = new TableInfoDto();
             tableInfoDto.setTableName(model.getTableName());
@@ -315,8 +329,8 @@ public class FeTunnelConfigServiceImpl implements FeTunnelConfigService {
             OriginalModel originalModel = finalHdfsMap.get(model.getTableName());
             tableInfoDto.setHdfsPath(Objects.isNull(originalModel) ? null : originalModel.getStoragePath());
             tableInfoDto.setHiveFileType(finalHiveConfigMap.get(model.getTableName()));
-            tableInfoDto.setWriterColumns(isHive ? doGetHiveColumns(modelColumnMap,model,Objects.isNull(originalModel) ? null : originalModel.getDataType()) : model.getColumnField() );
-            if (tunnel.getColType().equals(TunnelColTypeEnum.FREQUENCY_INCREMENT.getValue())){
+            tableInfoDto.setWriterColumns(isHive ? doGetHiveColumns(modelColumnMap, model, Objects.isNull(originalModel) ? null : (finalOriginalTableMap.get(originalModel.getOriginalId()) == null ? null : finalOriginalTableMap.get(originalModel.getOriginalId()).getDataSource())) : model.getColumnField());
+            if (tunnel.getColType().equals(TunnelColTypeEnum.FREQUENCY_INCREMENT.getValue())) {
                 // 1-时间 2-序列
                 tableInfoDto.setSeqField(model.getConditionField());
                 tableInfoDto.setSeqFieldType(model.getConditionFieldType());
@@ -332,12 +346,12 @@ public class FeTunnelConfigServiceImpl implements FeTunnelConfigService {
     /**
      * 创建hive库columns映射关系
      */
-    private String doGetHiveColumns(Map<String, List<OriginalModelColumn>> modelColumnMap,ConvCollectField field,String dataType) {
+    private String doGetHiveColumns(Map<String, List<OriginalModelColumn>> modelColumnMap, ConvCollectField field, Integer dataSource) {
         List<OriginalModelColumn> originalModelColumns = modelColumnMap.get(field.getTableName());
-        if (CollectionUtil.isEmpty(originalModelColumns)){
+        if (CollectionUtil.isEmpty(originalModelColumns)) {
             return null;
         }
-        if (StringUtils.isNotEmpty(field.getColumnField())){
+        if (StringUtils.isNotEmpty(field.getColumnField())) {
             List<String> existNames = Arrays.asList(field.getColumnField().split(","));
             originalModelColumns = originalModelColumns.stream().filter(column -> existNames.contains(column.getNameEn()))
                     .sorted(Comparator.comparing(OriginalModelColumn::getSeqNo)).collect(Collectors.toList());
@@ -345,8 +359,14 @@ public class FeTunnelConfigServiceImpl implements FeTunnelConfigService {
         StringBuilder sb = new StringBuilder();
         //dataType=1为采集标准数据 write全部转换为varchar
         for (OriginalModelColumn modelColumn : originalModelColumns) {
+            String dataType = null;
+            if (dataSource == null || dataSource != 1){
+                dataType = transformDataType(modelColumn.getFieldType());
+            }else {
+                dataType = transformDataType("varchar");
+            }
             sb.append("{\"name\":\"").append(modelColumn.getNameEn())
-                    .append("\",\"type\":\"").append(transformDataType("1".equals(dataType) ? "varchar" : modelColumn.getFieldType()))
+                    .append("\",\"type\":\"").append(dataType)
                     .append("\"}").append(",\n");
         }
         return sb.substring(0, sb.length() - 2);
@@ -355,40 +375,40 @@ public class FeTunnelConfigServiceImpl implements FeTunnelConfigService {
     private String transformDataType(String fieldType) {
         fieldType = fieldType.toLowerCase();
         String transformStr;
-       switch (fieldType){
-           case "varchar":
-           case "text":
-           case "json":
-               transformStr = "VARCHAR";
-               break;
-           case "int2":
-           case "int4":
-               transformStr = "INT";
-               break;
-           case "int8":
-               transformStr = "BIGINT";
-               break;
-           case "datetime":
-               transformStr = "DATE";
-               break;
-           case "numeric":
-               transformStr = "DOUBLE";
-               break;
-           case "float8":
-               transformStr = "FLOAT";
-               break;
-           case "timestamp":
-           case "timestampz":
-               transformStr = "TIMESTAMP";
-               break;
-           default:
-               transformStr = fieldType;
-               break;
-       }
-       return transformStr;
+        switch (fieldType) {
+            case "varchar":
+            case "text":
+            case "json":
+                transformStr = "VARCHAR";
+                break;
+            case "int2":
+            case "int4":
+                transformStr = "INT";
+                break;
+            case "int8":
+                transformStr = "BIGINT";
+                break;
+            case "datetime":
+                transformStr = "DATE";
+                break;
+            case "numeric":
+                transformStr = "DOUBLE";
+                break;
+            case "float8":
+                transformStr = "FLOAT";
+                break;
+            case "timestamp":
+            case "timestampz":
+                transformStr = "TIMESTAMP";
+                break;
+            default:
+                transformStr = fieldType;
+                break;
+        }
+        return transformStr;
     }
 
-    private void getIncrFieldMap(ConvTunnel tunnel, ConvCollectField field, Map<String, String> fieldMap){
+    private void getIncrFieldMap(ConvTunnel tunnel, ConvCollectField field, Map<String, String> fieldMap) {
         String column = field.getConditionField();
         ConvCollectIncrTime collectIncrTime = incrTimeService.getOne(new LambdaQueryWrapper<ConvCollectIncrTime>()
                 .eq(ConvCollectIncrTime::getTunnelId, tunnel.getId())
@@ -396,36 +416,36 @@ public class FeTunnelConfigServiceImpl implements FeTunnelConfigService {
                 .eq(ConvCollectIncrTime::getTableName, field.getTableName()));
         if (ObjectUtil.isNotNull(collectIncrTime)) {
             // 已进行增量采集，使用最新采集时间
-            if (SeqFieldTypeEnum.TIME.getValue().equals(field.getConditionFieldType())){
+            if (SeqFieldTypeEnum.TIME.getValue().equals(field.getConditionFieldType())) {
                 fieldMap.put(column, collectIncrTime.getLatestTime());
-            }else {
+            } else {
                 fieldMap.put(column, collectIncrTime.getLatestSeq());
             }
-        }else {
+        } else {
             // 第一次增量采集，使用初始点位
-            if (SeqFieldTypeEnum.TIME.getValue().equals(field.getConditionFieldType())){
+            if (SeqFieldTypeEnum.TIME.getValue().equals(field.getConditionFieldType())) {
                 LocalDateTime timePoint = field.getTimePoint();
                 String format = DateUtil.format(timePoint, "yyyy-MM-dd HH:mm:ss");
                 fieldMap.put(column, format);
-            }else {
+            } else {
                 fieldMap.put(column, String.valueOf(field.getNumPoint()));
             }
         }
     }
 
 
-    private Integer getCronTimeUnit(Integer timeDif, String timeUnit){
+    private Integer getCronTimeUnit(Integer timeDif, String timeUnit) {
         int seconds = 0;
         // 转换时间为毫秒
-        if (CharSequenceUtil.isBlank(timeUnit)){
+        if (CharSequenceUtil.isBlank(timeUnit)) {
             return seconds;
         }
         //小时
-        if (timeUnit.equals("h")){
+        if (timeUnit.equals("h")) {
             seconds = timeDif * 3600;
         }
         //分钟
-        if (timeUnit.equals("m")){
+        if (timeUnit.equals("m")) {
             seconds = timeDif * 60;
         }
         return seconds * 1000;
